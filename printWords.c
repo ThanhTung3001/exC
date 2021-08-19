1f3d90c19cf05d1d50feb46db4f89ac8b4ccb4f7
ThanhTung3001 <tungle3001.itfreelancer@gmail.com>
2021-08-19 07:31:07 +0700

    first commit

---
+++ printWords.c
@@ -0,0 +1,15 @@
+// printWords.c ... implementation of printWords function
+
+#include <assert.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "Trie.h"
+
+void printWords(Trie t) {
+    
+   //TODO
+}
+
